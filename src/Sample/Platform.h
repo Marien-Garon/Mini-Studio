779dadf68579cfd77c1516f4b70cec35fa3e646f
myoussouf-Y18 <myoussouf@gaming.tech>
2026-03-11 20:08:05 +0100

    Update Platform.h

--- src/Sample/Platform.h
+++ src/Sample/Platform.h
@@ -1,10 +1,15 @@
 #pragma once
 #include "Entity.h"
+#include "PhysicalEntity.h"
 
-class Platform : public Entity {
+class Platform : public Entity 
+{
+protected:
+    AABBCollider collider;
 public:
     virtual ~Platform() {};
 
+    virtual void CheckCollision(AABBCollider& playerCollider,PhysicalEntity* player) = 0;
     
     virtual void OnUpdate(float dt);
 
