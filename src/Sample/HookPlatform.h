cc0a0ba2d7b4741a4a5e44c6fa8cf33d233f1600
Ethan ODIET <eodiet@gaming.tech>
2026-03-13 09:31:14 +0100

    UnderPlat et hookplat finie

--- src/Sample/HookPlatform.h
+++ src/Sample/HookPlatform.h
@@ -1,6 +1,16 @@
 #pragma once
 #include "Platform.h"
+
+class Hook;
+
 class HookPlatform : public Platform
 {
-};
+private : 
+	Hook* m_platformHook;
+	int m_direction;
 
+public : 
+	void OnInitialize() override;
+
+	void SetPlateformDirection(int direction = 1);
+};
