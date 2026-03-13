0fc59ac6711c4344a7a88c58c1fb8911048d8839
Ethan ODIET <eodiet@gaming.tech>
2026-03-13 09:45:09 +0100

    fix hook plateform

--- src/Sample/HookPlatform.cpp
+++ src/Sample/HookPlatform.cpp
@@ -5,10 +5,10 @@ void HookPlatform::OnInitialize()
 {
 	m_platformHook = CreateEntity<Hook>(10, 10, sf::Color::Yellow);
 	if (m_direction == 1) {
-		m_platformHook.SetPosition(GetPosition(0.f, 0.f).x - 10, GetPosition(0.f, 0.f).y - 10);
+		m_platformHook->SetPosition(GetPosition(0.f, 0.f).x - 10, GetPosition(0.f, 0.f).y - 10);
 	}
 	else {
-		m_platformHook.SetPosition(GetPosition(1.f, 0.f).x + 10, GetPosition(0.f, 0.f).y + 10);
+		m_platformHook->SetPosition(GetPosition(1.f, 0.f).x + 10, GetPosition(0.f, 0.f).y + 10);
 	}
 }
 
