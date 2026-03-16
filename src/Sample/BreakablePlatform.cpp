61d69ff9e84ef93fd81b504b7726be1004518467
Mattéo YOUSSOUF <myoussouf@gaming.tech>
2026-03-16 13:50:35 +0100

    Update BreakablePlatform.cpp

--- src/Sample/BreakablePlatform.cpp
+++ src/Sample/BreakablePlatform.cpp
@@ -4,14 +4,8 @@
 
 void BreakablePlatform::OnCollision(Entity* entity)
 {
-	std::cout << "Side = " << (int)GetCollidingSide(entity) << std::endl;
-	std::cout << mShape.getPosition().x << "/" << mShape.getPosition().y << std::endl;
-	std::cout << entity->GetShape()->getPosition().x << "/" << entity->GetShape()->getPosition().y + entity->GetShape()->getGlobalBounds().height << std::endl;
-
 	if (GetCollidingSide(entity) == Side::UP)
 	{
-	    std::cout << "Timer : " << m_timer << std::endl;
-
 	    m_timer -= GetDeltaTime();
 
 		if (m_timer <= 0.5f)
