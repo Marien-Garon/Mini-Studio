7b37c493c565d9e82b7fb18cc8e0efca54a60376
Mailie GARCIA <mgarcia@gaming.tech>
2026-03-19 18:35:32 +0100

    added files

---
+++ src/Sample/DeathScene.cpp
@@ -0,0 +1,35 @@
+#include "DeathScene.h"
+
+#include "Player.h"
+#include "Scene.h"
+#include "Enemy.h"
+#include "Companion.h"
+#include "Button.h"
+#include "SceneManager.h"
+#include "AssetManager.h"
+
+void DeathScene::OnInitialize()
+{
+	AssetManager& AM = AssetManager::getInstance();
+	AM.StopMusic();
+
+	m_deathScreen = CreateEntity<Entity>(AM.LoadSprite("Balladeur"), sf::Color::Red);
+	//m_deathScreen->SetSpriteScale(0.333, 0.333);
+	m_deathScreen->SetPosition(GetWindowWidth(), GetWindowHeight(), 1.f, 1.f);
+
+	m_quitButton = CreateEntity<Button>(110, 50, sf::Color::Transparent);
+	m_replayButton = CreateEntity<Button>(110, 50, sf::Color::Transparent);
+
+	m_replayButton->SetFunction([]() {
+
+		SceneManager& SM = SceneManager::getInstance();
+		SM.ChangeScene("LEVEL");
+		});
+
+	m_quitButton->SetFunction([]() {
+
+		SceneManager& SM = SceneManager::getInstance();
+		exit(1);
+		});
+
+}
