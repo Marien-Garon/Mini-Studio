aaf42fb23d4d2b406d720a860cd9589975a046b6
Mailie GARCIA <mgarcia@gaming.tech>
2026-03-19 13:24:58 +0100

    test

--- src/Sample/main.cpp
+++ src/Sample/main.cpp
@@ -1,6 +1,7 @@
 #include "SampleScene.h"
 #include "GameManager.h"
 #include "LevelEditor.h"
+//#include "MenuScene.h"
 
 int main()
 {
@@ -10,10 +11,17 @@ int main()
 
 	pInstance->CreateWindow(1280, 720, "Sample", 60, {0,0,0}, true);
 
-	if (LaunchLevelEditor)
-		pInstance->LaunchScene<LevelEditor>();
-	else
-		pInstance->LaunchScene<SampleScene>();
+	MenuScene menuScene;
+	SampleScene sampleScene;
+	PauseScene pauseScene;
+	DeathMenu deathMenu;
+
+	sm.AddScene("START", &menuScene);
+	sm.AddScene("LEVEL", &sampleScene);
+	sm.AddScene("PAUSE", &pauseScene);
+	sm.AddScene("DEATH", &deathMenu);
+
+	sm.SetStartScene("START");
 
 
 	return 0;
